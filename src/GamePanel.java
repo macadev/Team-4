d93fa1377e362dcb027fd1ff60bd3527ef2f911e
macadev <danielmacario5@gmail.com>
2014-10-29 23:09:13 -0400

    initial menu implementation

---
+++ src/GamePanel.java
@@ -0,0 +1,117 @@
+/**
+ * Created by danielmacario on 14-10-29.
+ */
+
+import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.event.*;
+
+public class GamePanel extends JPanel implements Runnable, KeyListener {
+
+    public static final int WIDTH = 320;
+    public static final int HEIGHT = 240;
+    public static final int SCALE = 2;
+
+    // game thread
+    private Thread thread;
+    private boolean running;
+    private int FPS = 60;
+    private long targetTime = 1000 / FPS;
+
+    //image
+    private BufferedImage image;
+    private Graphics2D g;
+
+    // game state manager instance
+    private GameStateManager gsm;
+
+    public GamePanel() {
+        super();
+        setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+        setFocusable(true);
+        setBackground(Color.BLACK);
+        requestFocus();
+    }
+
+
+    public void addNotify() {
+        super.addNotify();
+        if (thread == null) {
+            thread = new Thread(this);
+            addKeyListener(this);
+            thread.start();
+        }
+    }
+
+
+    public void run() {
+        long start;
+        long elapse;
+        long wait;
+
+        init();
+
+        //game loop
+        while (running) {
+
+            g.setPaint(new Color(0,0,200));
+            g.fillRect(0, 0, image.getWidth(), image.getHeight() );
+            start = System.nanoTime();
+
+            update();
+            draw();
+            drawToScreen();
+
+            elapse  = System.nanoTime() - start;
+
+            wait = Math.abs(targetTime - elapse / 1000000);
+
+            try {
+                Thread.sleep(wait);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    private void init() {
+        image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+        g = image.createGraphics();
+        g.setPaint(new Color(0,0,200));
+        g.fillRect(0, 0, image.getWidth(), image.getHeight() );
+        running = true;
+        gsm = new GameStateManager();
+    }
+
+    private void update() {
+        gsm.update();
+    }
+
+    private void draw() {
+        gsm.draw(g);
+    }
+
+    private void drawToScreen() {
+        Graphics g2 = getGraphics();
+        g2.drawImage(image, 0, 0, WIDTH * SCALE, HEIGHT * SCALE, null);
+        g2.dispose();
+    }
+
+    public void keyTyped(KeyEvent key) {
+
+    }
+
+    public void keyPressed(KeyEvent key) {
+        gsm.keyPressed(key.getKeyCode());
+    }
+
+    public void keyReleased(KeyEvent key) {
+        gsm.keyReleased(key.getKeyCode());
+    }
+
+
+
+}
---
+++ src/MenuState.java
@@ -0,0 +1,85 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+/**
+ * Created by danielmacario on 14-10-29.
+ */
+public class MenuState extends GameState {
+
+    private String[] options = {"Start", "Help", "Quit"};
+    private int currentChoice = 0;
+    private Color titleColor;
+    private Font titleFont;
+    private Font font;
+
+    public MenuState(GameStateManager gsm) {
+        this.gsm = gsm;
+        titleColor = new Color(230, 200, 0);
+        titleFont = new Font("Century Gothic", Font.PLAIN, 28);
+        font = new Font("Arial", Font.PLAIN, 12);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+
+        //draw the title
+        g.setColor(titleColor);
+        g.setFont(titleFont);
+        g.setPaint(new Color(255,255,255));
+        g.drawString("Bomberman!", 80, 70);
+
+        //draw menu options
+        g.setFont(font);
+        for(int i = 0; i < options.length; i++) {
+            if (i == currentChoice) {
+                g.setColor(Color.GREEN);
+            } else {
+                g.setColor(Color.RED);
+            }
+            g.drawString(options[i], 145, 140 + i * 15);
+        }
+
+    }
+
+    private void select() {
+        if (currentChoice == 0) {
+            gsm.setState(GameStateManager.GAMEPLAY);
+        }
+        if (currentChoice == 1) {
+            //help
+        }
+        if(currentChoice == 2) {
+            System.exit(0);
+        }
+    }
+
+    @Override
+    public void keyPressed(int k) {
+        if (k == KeyEvent.VK_ENTER) {
+            select();
+        }
+        if (k == KeyEvent.VK_UP) {
+            currentChoice--;
+            if (currentChoice == -1) currentChoice  = options.length - 1;
+        }
+        if (k == KeyEvent.VK_DOWN) {
+            currentChoice++;
+            if (currentChoice == options.length) currentChoice = 0;
+        }
+    }
+
+    @Override
+    public void keyReleased(int k) {
+
+    }
+}
