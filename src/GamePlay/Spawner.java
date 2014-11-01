2767a51f988f45c9353158ece98f12f4f8f115de
macadev <danielmacario5@gmail.com>
2014-11-01 14:17:18 -0400

    created spawner skeleton

---
+++ src/GamePlay/Spawner.java
@@ -0,0 +1,24 @@
+package GamePlay;
+
+/**
+ * Created by danielmacario on 14-11-01.
+ */
+public class Spawner {
+
+    public void generateConcreteWalls() {
+
+    }
+
+    public void generateBrickWalls() {
+
+    }
+
+    public void generateEnemies() {
+
+    }
+
+    public void generateSetOfHarderEnemies() {
+
+    }
+
+}
