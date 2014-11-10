6b1007a09ee5284cd50d737e6cca2f5477afb7e8
shababahmed <shabab.ahmed@mail.mcgill.ca>
2014-11-10 01:29:23 -0400

    Fixed problem in Game.java

--- src/SystemController/Game.java
+++ src/SystemController/Game.java
@@ -17,35 +17,19 @@ public class Game {
         try {
             DatabaseController.initializeDatabase();
             System.out.println("Database succesfully initialized");
-<<<<<<< HEAD
-            db.createNewUser("John", "Johnpass","JohnRealName");
-            db.createNewUser("Jane", "Janepass","JaneRealName");
-            db.createNewUser("Bob","Bobpass","BobRealName");
-            if(db.authenticateUser("John","Johnpass")) {
-=======
             DatabaseController.createNewUser("m", "a","John");
             DatabaseController.createNewUser("testNewUser", "testNewPass","John");
             DatabaseController.createNewUser("testNewUser2", "testNewPass2","Jane");
             if (DatabaseController.authenticateUser("m","a")) {
->>>>>>> origin/master
                 System.out.println("Authentication Succesful");
             }
             else {
                 System.out.println("User not found");
             }
-<<<<<<< HEAD
-            if (db.updateInformation("johnNewPass","Johnny","John")); {
-                System.out.println("User info updated");
-            }
-        }
-
-        catch (ClassNotFoundException e) {
-=======
 
             DatabaseController.printDBContents();
 
         } catch (ClassNotFoundException e) {
->>>>>>> origin/master
             e.printStackTrace();
         }
         //test code for database
