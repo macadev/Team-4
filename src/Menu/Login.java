13d422db530ed7e1485af9d1459c338ef0dd8590
FlowFlorent <florent_lefebvre_1@hotmail.com>
2014-11-10 21:54:39 -0500

    Merging conflicts - no more popup menu

--- src/Menu/Login.java
+++ src/Menu/Login.java
@@ -1,5 +1,9 @@
 package Menu;
 
+
+import Database.DatabaseController;
+
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 
@@ -13,14 +17,25 @@ public class Login extends MenuTemplate {
     private Color titleColor;
     private Font titleFont;
     private Font font;
+    private JLabel labelMessage;
+    private JTextField fieldName;
+    private JPasswordField fieldPass;
+    private MenuManager menuManager;
+
 
     public Login (MenuManager menuManager) {
 
 
         this.menuManager = menuManager;
         titleColor = new Color(230, 200, 0);
-        titleFont = new Font("Gill Sans Ultra Bold", Font.PLAIN, 28);
-        font = new Font("Gill Sans Ultra Bold", Font.PLAIN, 12);
+
+        titleFont = new Font("Century Gothic", Font.PLAIN, 28);
+        font = new Font("Arial", Font.PLAIN, 12);
+        labelMessage = new JLabel("");
+        fieldName = new JPlaceHolderTextField();
+        fieldName.setPreferredSize(new Dimension(FIELD_WIDTH, FIELD_HEIGHT));
+        fieldPass = new JPlaceHolderPasswordField();
+        fieldPass.setPreferredSize(new Dimension(FIELD_WIDTH, FIELD_HEIGHT));
 
     }
 
@@ -58,10 +73,7 @@ public class Login extends MenuTemplate {
     private void select() {
         if (currentChoice == 0) {
 
-//            LoginPopup loginPopUp = new LoginPopup(menuManager);
-//            loginPopUp.setVisible(true);
-            menuManager.setMenuState(MenuState.MAIN);
-
+            loginClicked();
         }
         if (currentChoice == 1) {
             AccountCreationMenuPopUp acp = new AccountCreationMenuPopUp(menuManager);
@@ -91,4 +103,40 @@ public class Login extends MenuTemplate {
     public void keyReleased(int k) {
 
     }
+
+    public void loginClicked() {
+        String name = fieldName.getText();
+        //System.out.println(name);
+        String password = fieldPass.getText();
+        //System.out.println(password);
+        boolean loginSuccessful = false;
+
+        if (name.isEmpty()){
+            labelMessage.setText("Username cannot be blank");
+        } else if (password.isEmpty()){
+            labelMessage.setText("Password cannot be blank");
+        } else {
+            try {
+                loginSuccessful = DatabaseController.authenticateUser(name, password);
+            } catch (ClassNotFoundException e1) {
+                e1.printStackTrace();
+            }
+
+            if (loginSuccessful) {
+                labelMessage.setText("Success");
+                menuManager.associatePlayerUserName(name);
+                redirectToMainMenu();
+                setVisible(false);
+                dispose();
+            } else {
+                labelMessage.setText("Username-Password Combination Invalid");
+            }
+        }
+    }
+
+    public void redirectToMainMenu() {
+        menuManager.setMenuState(MenuState.MAIN);
+    }
+
 }
+
