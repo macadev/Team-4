7715147876f58790e6cdd474bf3f104ea28b4a8b
Vasu Gupta <vasundhara.gupta@mail.mcgill.ca>
2014-11-14 13:47:01 -0500

    updated stages 6-9

--- src/GameObject/Stages.java
+++ src/GameObject/Stages.java
@@ -1,5 +1,6 @@
 /**
  * Created by danielmacario on 14-11-12.
+ * Created onwards from stage 6: Vasundhara Gupta November 14,2014
  */
 package GameObject;
 
@@ -8,55 +9,67 @@ package GameObject;
  */
 public class Stages {
 
-    //An array used to contain the data for all 50 stages present in the game.
-    //Note that the first element is null, so that we can index the stages from 1.
-    public static final StageData[] gameStages =
-    {
-        null,
+	// An array used to contain the data for all 50 stages present in the game.
+	// Note that the first element is null, so that we can index the stages from
+	// 1.
+	public static final StageData[] gameStages = {
+			null,
 
-        new StageData(
-            new EnemySet[] { new EnemySet(EnemyType.BALLOOM, 6)},
-            PowerUpType.FLAMES, false
-        ),
+			new StageData(
+					new EnemySet[] { new EnemySet(EnemyType.BALLOOM, 6) },
+					PowerUpType.FLAMES, false),
 
-        new StageData(
-            new EnemySet[] { new EnemySet(EnemyType.BALLOOM, 3),
-            new EnemySet(EnemyType.ONEAL, 3)},
-            PowerUpType.BOMBS, false
-        ),
-        new StageData(
-            new EnemySet[] { new EnemySet(EnemyType.BALLOOM, 2),
-            new EnemySet(EnemyType.ONEAL, 2),
-            new EnemySet(EnemyType.DOLL, 2)},
-            PowerUpType.DETONATOR, false
-        ),
-        new StageData(new EnemySet[] {
-            new EnemySet(EnemyType.BALLOOM, 1),
-            new EnemySet(EnemyType.ONEAL, 1),
-            new EnemySet(EnemyType.DOLL, 2),
-            new EnemySet(EnemyType.MINVO, 2)},
-            PowerUpType.SPEED, false
-        ),
-        new StageData(new EnemySet[] {
-            new EnemySet(EnemyType.ONEAL, 4),
-            new EnemySet(EnemyType.DOLL, 3)},
-        PowerUpType.BOMBS, false
-        ),
+			new StageData(new EnemySet[] { new EnemySet(EnemyType.BALLOOM, 3),
+					new EnemySet(EnemyType.ONEAL, 3) }, PowerUpType.BOMBS,
+					false),
+			new StageData(new EnemySet[] { new EnemySet(EnemyType.BALLOOM, 2),
+					new EnemySet(EnemyType.ONEAL, 2),
+					new EnemySet(EnemyType.DOLL, 2) }, PowerUpType.DETONATOR,
+					false),
+			new StageData(new EnemySet[] { new EnemySet(EnemyType.BALLOOM, 1),
+					new EnemySet(EnemyType.ONEAL, 1),
+					new EnemySet(EnemyType.DOLL, 2),
+					new EnemySet(EnemyType.MINVO, 2) }, PowerUpType.SPEED,
+					false),
+			new StageData(new EnemySet[] { new EnemySet(EnemyType.ONEAL, 4),
+					new EnemySet(EnemyType.DOLL, 3) }, PowerUpType.BOMBS, false),
 
-        new StageData(new EnemySet[] {
-            new EnemySet(EnemyType.BALLOOM, 1)},
-            null,
-            true
-        ),
+			new StageData(
+					new EnemySet[] { new EnemySet(EnemyType.BALLOOM, 1) },
+					null, true),
 
-        new StageData(new EnemySet[] {
-            new EnemySet(EnemyType.ONEAL, 2),
-            new EnemySet(EnemyType.DOLL, 3),
-            new EnemySet(EnemyType.MINVO, 2)},
-            PowerUpType.BOMBS, false
-        ),
+			new StageData(new EnemySet[] { new EnemySet(EnemyType.ONEAL, 2),
+					new EnemySet(EnemyType.DOLL, 3),
+					new EnemySet(EnemyType.KONDORIA, 2) }, PowerUpType.FLAMES,
+					false),
 
+			new StageData(new EnemySet[] { new EnemySet(EnemyType.ONEAL, 2),
+					new EnemySet(EnemyType.DOLL, 3),
+					new EnemySet(EnemyType.KONDORIA, 2) }, PowerUpType.FLAMES,
+					false),
+			new StageData(new EnemySet[] { new EnemySet(EnemyType.ONEAL, 2),
+					new EnemySet(EnemyType.DOLL, 3),
+					new EnemySet(EnemyType.MINVO, 2) }, PowerUpType.BOMBS,
+					false),
+			new StageData(new EnemySet[] { new EnemySet(EnemyType.ONEAL, 1),
+					new EnemySet(EnemyType.DOLL, 2),
+					new EnemySet(EnemyType.MINVO, 4) }, PowerUpType.DETONATOR,
+					false),
 
-    };
+			new StageData(new EnemySet[] { new EnemySet(EnemyType.ONEAL, 1),
+					new EnemySet(EnemyType.DOLL, 1),
+					new EnemySet(EnemyType.MINVO, 4),
+					new EnemySet(EnemyType.KONDORIA, 2) },
+					PowerUpType.BOMBPASS, false),
+			new StageData(new EnemySet[] { new EnemySet(EnemyType.ONEAL, 2),
+					new EnemySet(EnemyType.DOLL, 3),
+					new EnemySet(EnemyType.KONDORIA, 2) }, PowerUpType.FLAMES,
+					false),
+			new StageData(new EnemySet[] { new EnemySet(EnemyType.ONEAL, 2),
+					new EnemySet(EnemyType.DOLL, 3),
+					new EnemySet(EnemyType.KONDORIA, 2) }, PowerUpType.FLAMES,
+					false),
+
+	};
 
 }
