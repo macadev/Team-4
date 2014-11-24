2f213c8b02125f06989b63574316444834402388
owenkun <owen.li@mail.mcgill.ca>
2014-11-23 22:39:15 -0500

    Test skeleton for database

---
+++ test/GameObject/Database/DatabaseControllerTest.java
@@ -0,0 +1,90 @@
+package GameObject.Database;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DatabaseControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testInitializeDatabase() throws Exception {
+
+    }
+
+    @Test
+    public void testPrintDBContents() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateNewUser() throws Exception {
+
+    }
+
+    @Test
+    public void testSetLevelUnlocked() throws Exception {
+
+    }
+
+    @Test
+    public void testGetLevelUnlocked() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateDirectoryForUserSavedFiles() throws Exception {
+
+    }
+
+    @Test
+    public void testAuthenticateUser() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdatePassword() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateRealName() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteAccount() throws Exception {
+
+    }
+
+    @Test
+    public void testGetScore() throws Exception {
+
+    }
+
+    @Test
+    public void testSetScore() throws Exception {
+
+    }
+
+    @Test
+    public void testGetTopUsers() throws Exception {
+
+    }
+
+    @Test
+    public void testGetScores() throws Exception {
+
+    }
+}
