2b977dc485698c06b1a60e492494241d2d543d1b
shababahmed <shabab.ahmed@mail.mcgill.ca>
2014-11-23 20:20:52 -0400

    Tested explode for BombTest. Created DoorTest skeleton

--- test/GameObject/BombTest.java
+++ test/GameObject/BombTest.java
@@ -1,10 +1,18 @@
 package GameObject;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BombTest {
+
+    private Bomb bomb;
+
+    @Before
+    public void makeBomb() {
+        bomb = new Bomb(35, 35);
+    }
 
     @Test
     public void testTimeExplosion() throws Exception {
@@ -13,7 +21,9 @@ public class BombTest {
 
     @Test
     public void testExplode() throws Exception {
-
+        assertTrue("Bomb is visible when placed", bomb.isVisible());
+        bomb.explode();
+        assertFalse("Bomb is no longer visible after exploding", bomb.isVisible());
     }
 
     @Test
--- test/GameObject/DoorTest.java
+++ test/GameObject/DoorTest.java
@@ -1,7 +1,18 @@
 package GameObject;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DoorTest {
 
+    @Test
+    public void testHitByExplosion() throws Exception {
+
+    }
+
+    @Test
+    public void testIsFirstCollision() throws Exception {
+
+    }
 }
