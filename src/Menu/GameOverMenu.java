fb48de757943acb0da66548759a44fc60ed147ce
Vasu Gupta <vasundhara.gupta@mail.mcgill.ca>
2014-11-24 20:19:32 -0500

    Finished commenting for In-Game +GameOver Menus

--- src/Menu/GameOverMenu.java
+++ src/Menu/GameOverMenu.java
@@ -15,7 +15,8 @@ import javax.swing.JTextField;
 public class GameOverMenu extends MenuTemplate {
 
     private String[] options = {"Start New Game","Load Saved Game", "View Leaderboard", "Return to Main Menu"};
-    private int currentChoice = 0;
+    // menu options as highlighted in the SRS
+    private int currentChoice = 0; // index of options array
     private Color titleColor = MenuTemplate.TITLE_COLOR;
     private Font titleFont = MenuTemplate.TITLE_FONT;
     private Font font = MenuTemplate.BODY_FONT;
@@ -31,7 +32,7 @@ public class GameOverMenu extends MenuTemplate {
         }
 
         @Override
-        public void draw(Graphics2D g) {
+        public void draw(Graphics2D g) { //the code in this method is used in every menu for consistency
             //draw the title
             g.setColor(titleColor);
             g.setFont(titleFont);
@@ -76,17 +77,21 @@ public class GameOverMenu extends MenuTemplate {
 
         public void select() {
             if (currentChoice == 0) {
-                LevelSelectionPopUp lsp = new LevelSelectionPopUp(menuManager);
+                //start new game
+                LevelSelectionPopUp lsp = new LevelSelectionPopUp(menuManager); // level selection pops up
                 lsp.setVisible(true);
             }
             if (currentChoice == 1) {
-                LoadGamePopUp acp = new LoadGamePopUp(menuManager);
+                //load saved game
+                LoadGamePopUp acp = new LoadGamePopUp(menuManager); //options of saved game pops up
                 acp.setVisible(true);
             }
             if (currentChoice == 2) {
+                //view leaderboard
                 menuManager.setMenuState(MenuState.LEADERBOARD);
             }
             if (currentChoice == 3) {
+                //return to main menu
                 menuManager.setMenuState(MenuState.MAIN);
             }
         }
