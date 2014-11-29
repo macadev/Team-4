8cb8688a1794d5a0c4b77f68548a0bc119ae15a3
shababahmed <shabab.ahmed@mail.mcgill.ca>
2014-11-29 06:13:03 -0400

    Created TileMapTest.java

---
+++ test/GameObject/TileMapTest.java
@@ -0,0 +1,35 @@
+package GameObject;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TileMapTest {
+    private TileMap tileMap;
+
+    @Before
+    public void Setup(){
+        
+    }
+
+    @Test
+    public void testDetermineIfShouldSpawnHarderEnemies() throws Exception {
+
+    }
+
+    @Test
+    public void testCountDownToHarderEnemySetSpawn() throws Exception {
+
+    }
+
+    @Test
+    public void testSpawnSetOfHarderEnemies() throws Exception {
+
+    }
+
+    @Test
+    public void testIncrementBombRadius() throws Exception {
+
+    }
+}
