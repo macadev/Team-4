f1d57c629273ab8fc6fd4fa4484dd9fd2e0f0532
macadev <danielmacario5@gmail.com>
2014-11-28 23:03:17 -0500

    added more cases to score manager tests, documented ScoreManager class

--- src/GamePlay/ScoreManager.java
+++ src/GamePlay/ScoreManager.java
@@ -2,42 +2,57 @@
  * Created by danielmacario on 14-11-17.
  */
 package GamePlay;
-import Database.DatabaseController;
+
 import GameObject.KillSet;
 
 import java.io.Serializable;
 import java.util.*;
 
-public class ScoreManager implements Serializable { 
+/**
+ * System used to calculate the scores obtained from killing enemies based
+ * on the specifications requested by the client.
+ */
+public class ScoreManager implements Serializable {
 
+    /**
+     * Main method of the class in charge of deciding the logic that will be used
+     * to calculate the score from the kills.
+     * @param enemiesKilled An ArrayList of KillSet objects specifying the type of
+     *                      enemy that was killed, where it died, and where the bomb
+     *                      that killed it was located.
+     * @return An integer representing the score obtained from the enemies killed
+     * by the player.
+     */
     public int determineScoreFromKills(ArrayList<KillSet> enemiesKilled) {
 
-        System.out.println(enemiesKilled.get(0).getScore());
+        // If only one enemy was killed, we return its score.
         if (enemiesKilled.size() == 1) return enemiesKilled.get(0).getScore();
 
         Hashtable<Coordinate, ArrayList<KillSet>> killsPerBomb = new Hashtable<Coordinate, ArrayList<KillSet>>();
 
+        // We group the enemies killed using a hash based on the origin of
+        // the bomb that killed them. The hash associates the coordinate
+        // where they were killed with an ArrayList that holds the KillSet
+        // object representing the enemy killed.
         for (KillSet deadEnemy : enemiesKilled) {
 
             Coordinate locationOfBomb = deadEnemy.getLocationOfBomb();
 
-            System.out.println("Bomb location COL = " + locationOfBomb.getCol() + " ROW = " + locationOfBomb.getRow());
-
             if (killsPerBomb.containsKey(locationOfBomb)) {
-                System.out.println("Adding to existing set!");
                 ArrayList<KillSet> bombKills = killsPerBomb.get(locationOfBomb);
                 bombKills.add(deadEnemy);
             } else {
-                System.out.println("New kill set");
                 ArrayList<KillSet> bombKills = new ArrayList<KillSet>();
                 bombKills.add(deadEnemy);
                 killsPerBomb.put(locationOfBomb, bombKills);
             }
-
         }
 
         Set<Coordinate> keys = killsPerBomb.keySet();
 
+        // For each of the groups of killed enemies, calculate the score
+        // obtained from each following the logic outlined in the
+        // requirements.
         int score = 0;
         for (Coordinate key: keys){
             ArrayList<KillSet> kills = killsPerBomb.get(key);
@@ -45,26 +60,20 @@ public class ScoreManager implements Serializable {
         }
 
         return score;
-
     }
 
+    /**
+     * Based on the number of killed enemies, calls a specific function
+     * to determine the score from the group of enemies killed.
+     * @param kills An ArrayList specifying the enemies killed by
+     *              a specific bomb.
+     * @return An integer representing the score obtained from
+     * the enemies killed by the player.
+     */
     public int calculateScore(ArrayList<KillSet> kills) {
         int score = 0;
 
-        //Sort the kills based on distance to the bomb
-        Collections.sort(kills, new Comparator<KillSet>() {
-            public int compare(KillSet o1, KillSet o2) {
-                return o1.getDistanceToBombOrigin() - o2.getDistanceToBombOrigin();
-            }
-        });
-
-        for (KillSet kill : kills) {
-            System.out.println("Is distance sorted : " + kill.getDistanceToBombOrigin());
-        }
-
-
         if (kills.size() == 2) {
-            System.out.println("Killed two enemies");
             score = determineScoreForTwoKills(kills.get(0), kills.get(1));
         } else {
             score = determineScoreForManyKills(kills);
@@ -73,8 +82,27 @@ public class ScoreManager implements Serializable {
         return score;
     }
 
+    /**
+     * Determines the score obtained when a single bomb kills more than 2 enemies.
+     * It first sorts the kills based on distance to the location of the bomb that
+     * killed them. And then it doubles the double for each successive kill.
+     * @param kills An ArrayList specifying the enemies killed by a specific bomb.
+     * @return An integer specifying the score obtained from killing more than two
+     * enemies with a single bomb blast.
+     */
     public int determineScoreForManyKills(ArrayList<KillSet> kills) {
 
+        // We sort the enemies killed based on their distance to the
+        // location of the bomb that killed them.
+        Collections.sort(kills, new Comparator<KillSet>() {
+            public int compare(KillSet o1, KillSet o2) {
+                return o1.getDistanceToBombOrigin() - o2.getDistanceToBombOrigin();
+            }
+        });
+
+        // As specified in the requirements, we double the double for each successive kill.
+        // For example, killing three Ballooms (score 100) results in a total score of
+        // 100 + 2 * 100 + 4 * 100 = 700
         int score = 0;
         int multiplier = 0;
         for (KillSet deadEnemy : kills) {
@@ -87,13 +115,22 @@ public class ScoreManager implements Serializable {
             multiplier *= 2;
         }
 
-
         return score;
     }
 
+    /**
+     * Determines the score obtained from killing two enemies.
+     * In this case, the lowest value kill gets doubled. Order
+     * of the enemies passed does not matter.
+     * @param killedEnemyA Enemy instance killed by the bomb blast.
+     * @param killedEnemyB Enemy instance killed by the bomb blast.
+     * @return An integer speicfying the score obtained from killing
+     * two enemies with a single bomb blast.
+     */
     public int determineScoreForTwoKills(KillSet killedEnemyA, KillSet killedEnemyB) {
         int killedEnemyAScore = killedEnemyA.getScore();
         int killedEnemyBScore = killedEnemyB.getScore();
+g
         if (killedEnemyAScore < killedEnemyBScore) {
             return killedEnemyAScore * 2 + killedEnemyBScore;
         }
--- test/GamePlay/ScoreManagerTest.java
+++ test/GamePlay/ScoreManagerTest.java
@@ -1,5 +1,7 @@
 package GamePlay;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,36 +16,162 @@ import static org.junit.Assert.*;
 public class ScoreManagerTest {
 
     private ArrayList<KillSet> kills = new ArrayList<KillSet>();
-    private ScoreManager sm = new ScoreManager();
+    private ScoreManager scoreManager = new ScoreManager();
 
     @Before
     public void setup() throws Exception {
         kills.add(new KillSet(new Coordinate(0, 0), new Coordinate(0, 0), new Enemy(EnemyType.BALLOOM, 35, 35)));
         kills.add(new KillSet(new Coordinate(0, 0), new Coordinate(0, 0), new Enemy(EnemyType.BALLOOM, 35, 35)));
-
     }
 
     @Test
     public void testDetermineScoreFromKills() throws Exception {
-        int result = sm.determineScoreFromKills(kills);
-        assertEquals(result, 300);
+        //Note that determineScoreFromKills is only called when at least one enemy has been killed.
+
+        //Enemies killed by bomb at (64,64)
+        // Oneal has score value of 200 - closest to bomb
+        KillSet killA = new KillSet(new Coordinate(32, 32), new Coordinate(64, 64), new Enemy(EnemyType.ONEAL, 32, 32));
+        // Kondoria has score value of 1000 - second closest to bomb
+        KillSet killB = new KillSet(new Coordinate(0, 0), new Coordinate(64, 64), new Enemy(EnemyType.KONDORIA, 32, 32));
+        // Balloom has score value of 100 - furthest from bomb
+        KillSet killC = new KillSet(new Coordinate(300, 300), new Coordinate(64, 64), new Enemy(EnemyType.BALLOOM, 32, 32));
+
+        //Enemies killed by bomb at (128,128)
+        // Balloom has score value of 100 - furthest from bomb
+        KillSet killD = new KillSet(new Coordinate(500, 500), new Coordinate(128, 128), new Enemy(EnemyType.BALLOOM, 32, 32));
+        // Balloom has score value of 100 - closest to bomb
+        KillSet killE = new KillSet(new Coordinate(300, 300), new Coordinate(128, 128), new Enemy(EnemyType.BALLOOM, 32, 32));
+        // Balloom has score value of 100 - second furthest from bomb
+        KillSet killF = new KillSet(new Coordinate(400, 400), new Coordinate(128, 128), new Enemy(EnemyType.BALLOOM, 32, 32));
+
+
+        ArrayList<KillSet> kills = new ArrayList<KillSet>();
+        kills.add(killA);
+        kills.add(killB);
+        kills.add(killC);
+        kills.add(killD);
+        kills.add(killE);
+        kills.add(killF);
+
+        int result = scoreManager.determineScoreFromKills(kills);
+        assertEquals("DetermineScoreFromKills should handle the case where multiple bombs kill enemies", result, 3300);
+
+        kills = new ArrayList<KillSet>();
+        kills.add(killA); // Score 200
+        kills.add(killB); // Score 1000
+        kills.add(killC); // Score 100
+
+        //200 + 2000 + 400
+        result = scoreManager.calculateScore(kills);
+        assertEquals("DetermineScoreFromKills should handle the case where only one bomb kills multiple enemies", result, 2600);
+
+        kills = new ArrayList<KillSet>();
+        kills.add(killA); // Oneal has score 200
+        result = scoreManager.calculateScore(kills);
+        assertEquals("DetermineScoreFromKills should only one enemy was killed", result, 200);
+
+        boolean exceptionThrown = false;
+        try {
+            scoreManager.determineScoreFromKills(null);
+        } catch (Exception e) {
+            exceptionThrown = true;
+        }
+        assertTrue("Passing a null value to determineScoreFromKills results in an exception being thrown", exceptionThrown);
+
     }
 
     @Test
     public void testCalculateScore() throws Exception {
-        int result = sm.calculateScore(kills);
-        assertEquals(result, 300);
+
+        // Oneal has score value of 200 - closest to bomb
+        KillSet killA = new KillSet(new Coordinate(32, 32), new Coordinate(64, 64), new Enemy(EnemyType.ONEAL, 32, 32));
+        // Kondoria has score value of 1000 - second closest to bomb
+        KillSet killB = new KillSet(new Coordinate(0, 0), new Coordinate(64, 64), new Enemy(EnemyType.KONDORIA, 32, 32));
+        // Balloom has score value of 100 - furthest from bomb
+        KillSet killC = new KillSet(new Coordinate(300, 300), new Coordinate(64, 64), new Enemy(EnemyType.BALLOOM, 32, 32));
+
+        ArrayList<KillSet> kills = new ArrayList<KillSet>();
+        kills.add(killA);
+        kills.add(killB);
+        kills.add(killC);
+
+        //200 + 2000 + 400
+        int result = scoreManager.calculateScore(kills);
+        assertEquals("For 3 or more enemies calculate score will sort the kills by distance to the bomb and " +
+                "then double the double for each successive kill", result, 2600);
+
+        //1000 + 200
+        kills.remove(0);
+        result = scoreManager.calculateScore(kills);
+        assertEquals("For just 2 enemies calculate score will sort the kills by distance to the bomb and " +
+                "then double the lowest value kill", result, 1200);
+
+        boolean exceptionThrown = false;
+        try {
+            scoreManager.determineScoreForTwoKills(killA, null);
+        } catch (Exception e) {
+            exceptionThrown = true;
+        }
+        assertTrue("Passing a null value to calculateScore results in an exception being thrown", exceptionThrown);
     }
 
     @Test
     public void testDetermineScoreForManyKills() throws Exception {
-        int result = sm.determineScoreForManyKills(kills);
-        assertEquals(result, 300);
+
+        // Oneal has score value of 200 - closest to bomb
+        KillSet killA = new KillSet(new Coordinate(32, 32), new Coordinate(64, 64), new Enemy(EnemyType.ONEAL, 32, 32));
+        // Kondoria has score value of 1000 - second closest to bomb
+        KillSet killB = new KillSet(new Coordinate(0, 0), new Coordinate(64, 64), new Enemy(EnemyType.KONDORIA, 32, 32));
+        // Balloom has score value of 100 - furthest from bomb
+        KillSet killC = new KillSet(new Coordinate(300, 300), new Coordinate(64, 64), new Enemy(EnemyType.BALLOOM, 32, 32));
+
+        ArrayList<KillSet> kills = new ArrayList<KillSet>();
+        kills.add(killA);
+        kills.add(killB);
+        kills.add(killC);
+
+        //200 + 2000 + 400
+        int result = scoreManager.calculateScore(kills);
+        assertEquals("DetermineScoreForManyKills will double the double for each successive kill", result, 2600);
+
+        kills = new ArrayList<KillSet>();
+        result = scoreManager.calculateScore(kills);
+        assertEquals("When 0 kills are passed, determineScoreForManyKills returns 0", result, 0);
+
+        boolean exceptionThrown = false;
+        try {
+            scoreManager.determineScoreForManyKills(null);
+        } catch (Exception e) {
+            exceptionThrown = true;
+        }
+
+        assertTrue("Passing a null value to determineScoreForManyKills results in an exception being thrown", exceptionThrown);
+
     }
 
     @Test
     public void testDetermineScoreForTwoKills() throws Exception {
-        int result = sm.determineScoreForTwoKills(kills.get(0), kills.get(1));
-        assertEquals(result, 300);
+
+        // Oneal has score value of 200
+        KillSet killA = new KillSet(new Coordinate(32, 32), new Coordinate(64, 64), new Enemy(EnemyType.ONEAL, 32, 32));
+        // Balloom has score value of 1000
+        KillSet killB = new KillSet(new Coordinate(32, 32), new Coordinate(64, 64), new Enemy(EnemyType.KONDORIA, 32, 32));
+
+        int result = scoreManager.determineScoreForTwoKills(killA, killB);
+        assertEquals("When two different enemy types are killed, the score of the kill with the lower " +
+                "value get doubled", 1400, result);
+
+        result = scoreManager.determineScoreForTwoKills(killA, killA);
+        assertEquals("When two enemies of the same type are killed, the score of the second enemy passed" +
+                "is doubled (it is arbitrary which one gets doubled)", 600, result);
+
+        boolean exceptionThrown = false;
+        try {
+            scoreManager.determineScoreForTwoKills(killA, null);
+        } catch (Exception e) {
+            exceptionThrown = true;
+        }
+
+        assertTrue("Passing a null value to DetermineScoreForTwoKills results in an exception being thrown", exceptionThrown);
     }
 }
